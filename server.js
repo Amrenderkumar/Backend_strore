dc54083178bdc472a0e370755ad00c665898c581
Amrender kuamr <amrenderk66@gmail.com>
2025-11-20 22:45:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('Hello Bahu');
+});
+
+const port = process.env.PORT || 2000;
+
+app.listen(port, () =>{
+    console.log(`server is ready, ${port}`)
+})
+
+app.get('/Don', (req, res) => {
+    res.send('<h1> Hello Amrender bahi kya hai kai </h1>')
+});
